29746df45bf15d3d8aec1da1aaa5f8bfe2a594a4
akhileshwarr <akhileshwar.rachakonda1@wipro.com>
2018-10-06 18:50:43 +0530

    pay is done

--- client/components/PayOutPlan/index.js
+++ client/components/PayOutPlan/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Offerings from '../Offerings';
 import './style.css';
 
 export default class PayOutPlan extends React.Component{
@@ -9,12 +8,11 @@ constructor(props){
     super(props);
     this.state= {
         edit: false,
-        creditValue:'200',
+        creditValue:'200'
     }
     this.handleEdit = this.handleEdit.bind(this);
     this.handleValue = this.handleValue.bind(this);
 }
-
 
 handleEdit() {
     this.setState({edit:!this.state.edit});
@@ -23,28 +21,28 @@ handleEdit() {
 handleValue(e) {
     this.setState({creditValue:e.target.value});
 }
-   
+
 makePaymentClick(){
   this.props.history.push('/offerings')
 }
     render(){
+      let context = this
         var payOffs = ''
         var payOutData = this.props.payOutData['creditDebitMatch'].map(function (data,i) {
           payOffs = data.senders.map(function (data1, i) {
             console.log(data1, 'daat1');
             return(
               <div className='row'>
-                  <p className='col-8 float-left'>{data1.accountTitle} (<span>&#163;</span> {data1.contributingAmount})</p>
-                  <p>(-) <span>&#163;</span> {data1.totalAvailableBalance} </p>
+                  <p className='col-8 float-left'>{data1.bankName} (<span>&#163;</span> {data1.beforeContributingBalance})</p>
+                  <p>(-) <span>&#163;</span> {data1.contributingAmount} </p>
               </div>
             )
           });
-        
+
         return(
             <div className="card col-xs-4 match-card">
                 <div className="card-header match-header">
-                <b className="float-left">{data.accountTitle}</b>
-
+                <b className="float-left">{data.accountTitle} ({data.bankName})</b>
                 {data.clearedTotalDue ? <i className="far fa-check-square fa-2x float-right" style={{color:"#0ACA24"}}></i>:null}
                 </div>
                 <div className="card-body">
@@ -61,16 +59,20 @@ makePaymentClick(){
                 <div className="card-footer match-footer">
                 <div className='row'>
                     <h6 className='col-8 float-left' style={{color:'#fff'}}>Balance Due</h6>
-                    <h6 style={{color:'#0ACA24'}} className="float-right"><span>&#163;</span>{data.clearedTotalDue ? 0 : data.totalAvailableBalance}</h6>
+                    <h6 style={{color:'#0ACA24'}} className="float-right"><span>&#163;</span>{data.clearedTotalDue ? 0 : context.props.payOutData.totalAvailableBalance}</h6>
                 </div>
                 </div>
             </div>
           )
         })
+        console.log(this.props, 'props');
+        var savings = this.props.payOutData.totalAvailableBalance < 0 ? 'deficit of' : 'savings'
+        var only = this.props.payOutData.totalAvailableBalance < 0 ? 'only' : ''
+        console.log(savings,'savings');
         return(
             <div className="container best-match-layout">
              <center>
-             <h4><b style={{color:'#0ACA24'}}>Congratulations</b></h4> <h6 style={{fontSize:'22px'}}>Found the Best Match for you, following which , you will be saving <span>&#163;</span>200</h6>   </center>
+             <h4><b style={{color:'#0ACA24'}}>Congratulations</b></h4> <h6 style={{fontSize:'22px'}}>Found the Best Match for you, following which , you will be {savings} <span>&#163;</span>{this.props.payOutData.totalAvailableBalance} {only}</h6></center>
             <div className="row">
             {payOutData}
             </div>
