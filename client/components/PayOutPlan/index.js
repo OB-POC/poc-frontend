423b14eb9db7d0d49e18bfcfe011a33c7dbd7618
akhileshwarr <akhileshwar.rachakonda1@wipro.com>
2018-10-06 20:38:39 +0530

    added editing

--- client/components/PayOutPlan/index.js
+++ client/components/PayOutPlan/index.js
@@ -7,15 +7,17 @@ export default class PayOutPlan extends React.Component{
 constructor(props){
     super(props);
     this.state= {
-        edit: false,
-        creditValue:'200'
+        // edit0: false,
+        // edit1: false,
+        // edit2: false,
+        edit: false
     }
     this.handleEdit = this.handleEdit.bind(this);
     this.handleValue = this.handleValue.bind(this);
 }
 
 handleEdit() {
-    this.setState({edit:!this.state.edit});
+    this.setState({edit: !this.state.edit});
 }
 
 handleValue(e) {
@@ -27,15 +29,25 @@ makePaymentClick(){
 }
     render(){
       let context = this
+      let contextState = this.state
         var payOffs = ''
         var payOutData = this.props.payOutData['creditDebitMatch'].map(function (data,i) {
-          payOffs = data.senders.map(function (data1, i) {
+          payOffs = data.senders.map(function (data1, j) {
+            var dVal = '(-) £ '+data1.contributingAmount
+            if(data1.contributingAmount == 0 && !contextState.edit)
+            return null
+            else{
             return(
               <div className='row'>
                   <p className='col-8 float-left'>{data1.bankName} (<span>&#163;</span> {data1.beforeContributingBalance})</p>
+                  {
+                    contextState.edit ? <input type='text' style = {{width: '100px', height: '20px'}}
+                    defaultValue = {dVal}/>:
                   <p>(-) <span>&#163;</span> {data1.contributingAmount} </p>
+                }
               </div>
             )
+          }
           });
 
         return(
@@ -51,7 +63,7 @@ makePaymentClick(){
                 </div>
                 <br/>
                 <div className='row'>
-                    <h6 className='col-8 float-left'>Payoff from : </h6><i className='fas fa-pen' style={{paddingLeft:'80px'}}></i>
+                    <h6 className='col-8 float-left'>Payoff from : </h6>
                 </div>
                 {payOffs}
                 </div>
@@ -64,14 +76,20 @@ makePaymentClick(){
             </div>
           )
         })
-        console.log(this.props, 'props');
         var savings = this.props.payOutData.totalAvailableBalance < 0 ? 'deficit of' : 'savings'
         var only = this.props.payOutData.totalAvailableBalance < 0 ? 'only' : ''
         console.log(savings,'savings');
         return(
             <div className="container best-match-layout">
              <center>
-             <h4><b style={{color:'#0ACA24'}}>Congratulations</b></h4> <h6 style={{fontSize:'22px'}}>Found the Best Match for you, following which , you will be {savings} <span>&#163;</span>{this.props.payOutData.totalAvailableBalance} {only}</h6></center>
+             <h4><b style={{color:'#0ACA24'}}>Congratulations</b></h4>
+             <h6 style={{fontSize:'22px'}}>Found the Best Match for you, following which , you will be {savings} <span>&#163;</span>{this.props.payOutData.totalAvailableBalance} {only}
+             {!this.state.edit ?
+               <i className='fas fa-pen float-right' style={{paddingLeft:'80px'}} onClick={context.handleEdit}></i>:
+               <i className='fas fa-check float-right' style={{paddingLeft:'80px',color: '#0ACA24'}} onClick={context.handleEdit}></i>
+             }
+             </h6>
+             </center>
             <div className="row">
             {payOutData}
             </div>
